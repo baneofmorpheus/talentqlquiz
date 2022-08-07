e94b6c82ba4ed0cce05a2bc2704bf5bb7aa47a48
Leon Chux <epicgenii18@gmail.com>
2022-08-07 18:24:36 +0100

    setup routes for project

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { query } = require('express-validator');
+
+const { rateLimiterUsingThirdParty } = require('../middleware/rateLimiter');
+const calculateAgeController = require('../controllers/CalculateAgeController');
+
+const router = express.Router();
+
+router.use(rateLimiterUsingThirdParty);
+router.get(
+  '/calculateAgeInYears',
+  query('dob').exists({ checkNull: true, checkFalsy: true }).isInt(),
+  calculateAgeController.calculateAgeInYears
+);
+
+module.exports = router;
