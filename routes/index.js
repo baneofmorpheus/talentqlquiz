9bdf71bfe920c5e0d8e0de87c12e8889e777d62a
Leon Chux <epicgenii18@gmail.com>
2022-08-07 22:58:47 +0100

    updated validation to handle edge cases

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,20 @@ const router = express.Router();
 router.use(rateLimiterUsingThirdParty);
 router.get(
   '/howold',
-  query('dob').exists({ checkNull: true, checkFalsy: true }).isInt(),
+  query('dob')
+    .exists({ checkNull: true, checkFalsy: true })
+    .isInt()
+    .custom((value) => {
+      /**
+       * Check if timestamp is valid date timestamp
+       */
+      const dateObject = new Date(value);
+
+      if (dateObject.getTime() > 0) {
+        return;
+      }
+      throw new Error('invalid dob format');
+    }),
   calculateAgeController.calculateAgeInYears
 );
 
