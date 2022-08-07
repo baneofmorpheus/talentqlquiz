eb91d6143a7020f9ae8512a947e59744c01159fa
Leon Chux <epicgenii18@gmail.com>
2022-08-07 18:26:16 +0100

    setup controller

---
+++ controllers/CalculateAgeController.js
@@ -0,0 +1,40 @@
+const { validationResult } = require('express-validator');
+
+exports.calculateAgeInYears = async (req, res) => {
+  try {
+    /**
+     * Handle validation errors
+     */
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    /**
+     * Get number of milliseconds in one year
+     */
+    const minute = 1000 * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const year = day * 365;
+
+    /**
+     * Convert timestamp to number from string and get birthDate object
+     */
+    const birthDate = new Date(parseInt(req.query.dob));
+    const currentDate = new Date();
+
+    const diffinDateInMilliseconds = currentDate - birthDate;
+
+    /**Convert to yearts */
+    const diffinDateInYears = Math.round(diffinDateInMilliseconds / year);
+
+    return res
+      .status(200)
+      .json({ msg: 'Age calculated successfully', age: diffinDateInYears });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
